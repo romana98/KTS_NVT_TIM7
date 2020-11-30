6c0e3b683a227c8e0295d91ff5b9f47930f6cbc1
2020_sweetovci <miloradradovicmico98@gmail.com>
2020-11-30 21:43:07 +0100

    genericki interfejs za service klase

--- tim7/tim7/src/main/java/com/project/tim7/service/LocationService.java
+++ tim7/tim7/src/main/java/com/project/tim7/service/LocationService.java
@@ -1,8 +1,40 @@
 package com.project.tim7.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class LocationService implements ServiceInterface {
+
+	@Override
+	public List findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object findOne(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean saveOne(Object entity) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean saveAll(List entities) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
