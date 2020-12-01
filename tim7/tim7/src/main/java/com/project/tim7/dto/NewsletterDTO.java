f8662ad8a97268be108aaf470815c06f8aad182a
verak13 <verakovacevic98@gmail.com>
2020-12-01 15:47:48 +0100

    dodavanje newsletter

---
+++ tim7/tim7/src/main/java/com/project/tim7/dto/NewsletterDTO.java
@@ -0,0 +1,89 @@
+package com.project.tim7.dto;
+
+import java.util.Date;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Pattern;
+
+public class NewsletterDTO {
+	
+	private int id;
+	
+	@NotBlank
+	private String name;
+	
+	private String description;
+	
+	@PastOrPresent
+	private Date publishedDate;
+	
+	private int culturalOfferId;
+	
+	@Pattern(regexp="([^\\s]+(\\.(?i)(jpe?g|png|gif|bmp))$)")
+	private String picture;
+
+	public NewsletterDTO(int id, @NotBlank String name, String description, @NotBlank @PastOrPresent Date publishedDate,
+			@NotBlank int culturalOfferId, @Pattern(regexp = "([^\\s]+(\\.(?i)(jpe?g|png|gif|bmp))$)") String picture) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.publishedDate = publishedDate;
+		this.culturalOfferId = culturalOfferId;
+		this.picture = picture;
+	}
+	
+	public NewsletterDTO() {}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getPublishedDate() {
+		return publishedDate;
+	}
+
+	public void setPublishedDate(Date publishedDate) {
+		this.publishedDate = publishedDate;
+	}
+
+	public int getCulturalOfferId() {
+		return culturalOfferId;
+	}
+
+	public void setCulturalOfferId(int culturalOfferId) {
+		this.culturalOfferId = culturalOfferId;
+	}
+
+	public String getPicture() {
+		return picture;
+	}
+
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+	
+	
+
+}
--- tim7/tim7/src/main/java/com/project/tim7/service/NewsletterService.java
+++ tim7/tim7/src/main/java/com/project/tim7/service/NewsletterService.java
@@ -2,39 +2,74 @@ package com.project.tim7.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.project.tim7.dto.NewsletterDTO;
+import com.project.tim7.model.CulturalOffer;
+import com.project.tim7.model.Newsletter;
+import com.project.tim7.model.Picture;
+import com.project.tim7.repository.CulturalOfferRepository;
+import com.project.tim7.repository.NewsletterRepository;
+import com.project.tim7.repository.PictureRepository;
+
 @Service
-public class NewsletterService implements ServiceInterface {
+public class NewsletterService implements ServiceInterface<Newsletter> {
+	
+	@Autowired
+	NewsletterRepository newsletterRepo;
+	
+	@Autowired 
+	CulturalOfferRepository culturalOfferRepo;
+	
+	@Autowired
+	PictureRepository pictureRepo;
 
 	@Override
-	public List findAll() {
-		// TODO Auto-generated method stub
+	public List<Newsletter> findAll() {
 		return null;
 	}
 
 	@Override
-	public Object findOne(int id) {
-		// TODO Auto-generated method stub
-		return null;
+	public Newsletter findOne(int id) {
+		return newsletterRepo.findById(id).orElse(null);
 	}
 
 	@Override
-	public boolean saveOne(Object entity) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean saveOne(Newsletter entity) {
+		if (findOne(entity.getId()) != null) 
+			return false;		
+		newsletterRepo.save(entity);
+		return true;
 	}
 
 	@Override
-	public boolean saveAll(List entities) {
-		// TODO Auto-generated method stub
+	public boolean saveAll(List<Newsletter> entities) {
 		return false;
 	}
 
 	@Override
 	public boolean delete(int id) {
-		// TODO Auto-generated method stub
 		return false;
+	}
+	
+	public boolean saveNewsletter(Newsletter entity, int culturalOfferId, String pictureStr) {
+		CulturalOffer culturalOffer = findOneCulturalOffer(culturalOfferId);
+		if (culturalOffer == null) 
+			return false;
+		entity.setCulturalOffer(culturalOffer);
+		
+		Picture picture = new Picture(pictureStr);
+		pictureRepo.save(picture);
+		entity.setPicture(picture);
+		
+		if (saveOne(entity))
+			return true;
+		return false;
+	}
+	
+	public CulturalOffer findOneCulturalOffer(int id) {
+		return culturalOfferRepo.findById(id).orElse(null);
 	}
 
 }
