67f583e42cb460ea252550194a141895b4defa14
verak13 <verakovacevic98@gmail.com>
2020-12-01 17:11:03 +0100

    prikaz svih newsletter-a

--- tim7/tim7/src/main/java/com/project/tim7/api/NewsletterController.java
+++ tim7/tim7/src/main/java/com/project/tim7/api/NewsletterController.java
@@ -1,8 +1,14 @@
 package com.project.tim7.api;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,8 +17,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.tim7.dto.CategoryDTO;
 import com.project.tim7.dto.NewsletterDTO;
 import com.project.tim7.helper.NewsletterMapper;
+import com.project.tim7.model.Category;
 import com.project.tim7.model.Newsletter;
 import com.project.tim7.service.NewsletterService;
 
@@ -41,5 +49,31 @@ public class NewsletterController {
         }
         return new ResponseEntity<>("Successfully added newsletter.", HttpStatus.CREATED);
     }
+    
+    @RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<NewsletterDTO>> getAllNewsletters(){
+		List<Newsletter> newsletters = newsletterService.findAll();
+		
+        return new ResponseEntity<>(toNewsletterDTOList(newsletters), HttpStatus.OK);
+
+	}
+    
+    @RequestMapping(value= "/by-page",method = RequestMethod.GET)
+    public ResponseEntity<Page<NewsletterDTO>> getAllNewsletters(Pageable pageable) {
+        Page<Newsletter> page = newsletterService.findAll(pageable);
+        List<NewsletterDTO> dtos = toNewsletterDTOList(page.toList());
+        Page<NewsletterDTO> pageNewsletterDTOS = new PageImpl<>(dtos,page.getPageable(),page.getTotalElements());
+
+        return new ResponseEntity<>(pageNewsletterDTOS, HttpStatus.OK);
+    }
+
+	private List<NewsletterDTO> toNewsletterDTOList(List<Newsletter> newsletters) {
+		ArrayList<NewsletterDTO> dtos = new ArrayList<NewsletterDTO>();
+		for(Newsletter newsletter : newsletters) {
+			NewsletterDTO dto = newsletterMapper.toDto(newsletter);
+			dtos.add(dto);
+		}
+		return dtos;
+	}
 
 }
