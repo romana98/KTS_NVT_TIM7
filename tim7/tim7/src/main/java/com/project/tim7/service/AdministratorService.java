86a252667f25cf53c2ef2d226baf0562a7e6dd2c
romana98 <romana_1998@hotmail.com>
2020-12-01 13:15:08 +0100

    added create admin

--- tim7/tim7/src/main/java/com/project/tim7/service/AdministratorService.java
+++ tim7/tim7/src/main/java/com/project/tim7/service/AdministratorService.java
@@ -2,39 +2,49 @@ package com.project.tim7.service;
 
 import java.util.List;
 
+import com.project.tim7.model.Administrator;
+import com.project.tim7.repository.AdministratorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AdministratorService implements ServiceInterface {
+public class AdministratorService implements ServiceInterface<Administrator> {
+
+	@Autowired
+	AdministratorRepository adminRepo;
+
 
 	@Override
-	public List findAll() {
-		// TODO Auto-generated method stub
+	public List<Administrator> findAll() {
 		return null;
 	}
 
 	@Override
-	public Object findOne(int id) {
-		// TODO Auto-generated method stub
-		return null;
+	public Administrator findOne(int id) {
+		return adminRepo.findById(id).orElse(null);
 	}
 
 	@Override
-	public boolean saveOne(Object entity) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean saveOne(Administrator entity) {
+		System.out.println(findByEmailorUsername(entity.getEmail(), entity.getUsername()));
+		if(!findByEmailorUsername(entity.getEmail(), entity.getUsername()).isEmpty())
+			return false;
+
+		adminRepo.save(entity);
+		return true;
 	}
 
 	@Override
-	public boolean saveAll(List entities) {
-		// TODO Auto-generated method stub
+	public boolean saveAll(List<Administrator> entities) {
 		return false;
 	}
 
 	@Override
 	public boolean delete(int id) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
+	public List<Administrator> findByEmailorUsername(String email, String username){
+		return adminRepo.findByEmailOrUsername(email, username);
+	}
 }
