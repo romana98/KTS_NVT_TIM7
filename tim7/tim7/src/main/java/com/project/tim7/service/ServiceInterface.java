664e7c2af83840f37bca7fe876d41b8efa08d462
2020_sweetovci <miloradradovicmico98@gmail.com>
2020-12-01 15:13:22 +0100

    fetching categories with paginator

--- tim7/tim7/src/main/java/com/project/tim7/service/ServiceInterface.java
+++ tim7/tim7/src/main/java/com/project/tim7/service/ServiceInterface.java
@@ -2,11 +2,16 @@ package com.project.tim7.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface ServiceInterface<T> {
 	
 	List<T> findAll();
 	
 	T findOne(int id);
+	
+	Page<T> findAll(Pageable pageable);
 	
 	boolean saveOne(T entity);
 	
