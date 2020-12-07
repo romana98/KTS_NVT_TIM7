1e3d7263d6b193494c472644083643e59707a230
2020_sweetovci <miloradradovicmico98@gmail.com>
2020-12-07 14:54:03 +0100

    dodata autentikacija usera pri dodavanju komentara

--- tim7/tim7/src/main/java/com/project/tim7/api/CommentController.java
+++ tim7/tim7/src/main/java/com/project/tim7/api/CommentController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -31,6 +32,7 @@ public class CommentController {
 		this.commentMapper = new CommentMapper();
 	}
 	
+	@PreAuthorize("hasRole('ROLE_REGISTERED')")
 	@RequestMapping(method= RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Object> createComment(@Valid @RequestBody CommentDTO commentDTO){
         
